7af938eb7bd6581401fb0113e8845608684cd953
ImDalyn <stilesd2588@student.faytechcc.edu>
2024-06-11 19:39:26 -0400

    Add files via upload

---
+++ P1LAB1_StilesDalyn.py
@@ -0,0 +1,5 @@
+# Dalyn Stiles # 06/11/2024 #P1LAB1_StilesDalyn #Using python to show print and input functions
+firstname=input("What is your first name?")
+lastname=input("What is your last name?")
+print("Hello,", firstname, lastname, "! Welcome to CTI-110")
+
