be786b97b943103bbf03f0c0bfc9bfed45d869a4
ImDalyn <stilesd2588@student.faytechcc.edu>
2024-06-12 15:07:03 -0400

    Add files via upload
    Second Submission

---
+++ P1HW1_StilesDalyn.py
@@ -0,0 +1,14 @@
+# Dalyn Stiles
+# 06/12/2024
+#P1HW1
+#Using two variables for exponents, Using 3 variables for addition and subtraction
+print("-----Calculating Exponents-----")
+num=int(input('Enter an integer aas the base value:'))
+expo=int(input('Enter an integer as the exponent:'))
+print(num, "raised to the power of", expo, "is", num**expo, "! !")
+
+print("-----Addition and Sutraction-----")
+num1=int(input('Enter a starting integer:'))
+num2=int(input("Enter an integer to add:"))
+num3=int(input('Enter an integer to subtract:'))
+print(num1, "+", num2, "-", num3,"is equal to", num1+num2-num3)
